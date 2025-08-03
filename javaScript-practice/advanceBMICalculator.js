abe5b213760d9bf354697102499ee23e6dedaaf8
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-08-03 16:03:37 +0530

    Create advanceBMICalculator.js

---
+++ javaScript-practice/advanceBMICalculator.js
@@ -0,0 +1,14 @@
+function bmiCalculator (weight, height) {
+    var bmi=weight/Math.pow(height,2);
+    var interpretation;
+    if(bmi>24.9){
+        interpretation="Your BMI is "+bmi+", so you are overweight.";
+    }
+    else if(bmi>18.5){
+        interpretation="Your BMI is "+bmi+", so you have a normal weight.";
+    }
+    else{
+        interpretation="Your BMI is "+bmi+", so you are underweight.";
+    }
+    return interpretation;
+}
