28be8296be0004153833dd07f8581b3d9fe3e77e
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-08-03 14:31:28 +0530

    Create fizzBuzzGame.js

---
+++ javaScript-practice/fizzBuzzGame.js
@@ -0,0 +1,20 @@
+var output=[];
+var i=1;
+function fizzBuzz(){
+  if(i%3===0 && i%5===0){
+    output.push("fizzBuzz");
+  }
+  else{
+    if(i%3===0){
+      output.push("fizz");
+    }
+    else if(i%5===0){
+      output.push("Buzz");
+    }
+    else{
+      output.push(i);
+    }
+  }
+  i++;
+  console.log(output);
+}
