9209a133b6ed55d2c30ff45e03b0e9eb8144e6f9
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-08-03 14:19:48 +0530

    Update loveCalculator.js using if else

--- javaScript-practice/loveCalculator.js
+++ javaScript-practice/loveCalculator.js
@@ -1,3 +1,14 @@
 prompt("What is your name?");
 prompt("What is your crush's name?");
-alert("Your Love Score is "+(Math.floor(Math.random()*100)+1)+"%.");
+// alert("Your Love Score is "+(Math.floor(Math.random()*100)+1)+"%.");
+
+var loveScore=Math.floor(Math.random()*100)+1;
+if (loveScore>=70) {
+  alert("Your love percentage is " + loveScore + "%. You guys are basically soulmates.");
+}
+else if (loveScore >= 30) {
+  alert("Your love percentage is " + loveScore + "%");
+}
+else{
+  alert("Your love percentage is " + loveScore + "%. You guys are like oil and water.");
+}
